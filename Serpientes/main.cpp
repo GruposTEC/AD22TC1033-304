b7a8a6e9bc10b06367b346cd4f2d8b37ec9d0721
Antonio  Mejia <avmejia@tec.mx>
2022-11-12 12:42:32 -0600

    Codepsaces

---
+++ Serpientes/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+int main()
+{
+    int prueba;
+    cout << "Dentro de Serpientes y Escaleras" << endl;
+    cin >> prueba;
+    cout << "Buena suerte" << endl;
+}
